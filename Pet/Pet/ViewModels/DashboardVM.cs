4180e1ae47416abd0b24fd9844e94e3dba7e6c44
Lam <roshi0188@gmail.com>
2026-05-31 10:39:16 +0700

    Hoan thanh caesar

---
+++ Pet/Pet/ViewModels/DashboardVM.cs
@@ -0,0 +1,24 @@
+﻿namespace PetSocial.ViewModels
+{
+    public class DashboardVM
+    {
+        public int TotalUsers { get; set; }
+        public int TotalPets { get; set; }
+        public int TotalPosts { get; set; }
+        public int TotalComments { get; set; }
+        public int TotalLikes { get; set; }
+        public int TotalMessages { get; set; }
+        public List<UserDashboardVM> Users { get; set; } = new List<UserDashboardVM>();
+    }
+
+    public class UserDashboardVM
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime JoinDate { get; set; }
+        public int PetCount { get; set; }
+        public int PostCount { get; set; }
+        public string Role { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
